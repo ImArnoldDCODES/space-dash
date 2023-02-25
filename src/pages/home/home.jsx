367bf6adc46d57efd6b827ab23687249fef4e10b
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-02-25 21:52:45 +0100

    hello

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import DesktopBg from "../../assets/home/background-home-desktop.jpg";
+
+export default function Home() {
+  return (
+    <div className="h-[100vh] w-[100vw] overflow-hidden home-page">
+      <div className="flex h-[100%] m-auto mt-12">
+        <div className="basis-[50%] m-auto text-start mt-[13rem] pl-[9rem]">
+          <h4 className="text-[#D0D6F9] text-[28px] font-[400] leading-normal tracking-wide">
+            SO, YOU WANT TO TRAVEL TO
+          </h4>
+          <h2 className="text-[#fff] text-[150px] font-[400] leading-normal">
+            SPACE
+          </h2>
+          <p className="w-[28rem] text-[#D0D6F9] text-[18px] font-[400] leading-normal">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="basis-[50%] m-auto mt-[20rem]">
+          <div className="bg-[#979797]/50 w-[20rem] h-[20rem] text-center text-[#fff] flex m-auto rounded-[50%]">
+            <div className="bg-[#fff] w-[12rem] h-[12rem] text-center text-[#000] flex m-auto rounded-[50%]">
+              <h6 className="m-auto">EXPLORE</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/navbar/navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Logo from "../../assets/shared/logo.svg";
+
+export default function Navbar() {
+  const navtext = [
+    {
+      title: "00 HOME",
+    },
+    {
+      title: "01 DESTINATION",
+    },
+    {
+      title: "02 CREW",
+    },
+    {
+      title: "03 TECHNOLOGY",
+    },
+  ];
+
+  return (
+    <div className="absolute flex w-[100%] justify-between items-center mt-10">
+      <div>
+        <img src={Logo} alt="logo" className="w-[3rem] h-[3rem] ml-12" />
+      </div>
+      <span className="h-[2px] w-[35rem] left-[24rem] opacity-[0.25] bg-[#fff] absolute content-none z-[1]"></span>
+      <div 
+        className="w-[38rem] h-[100%]"
+        style={{
+          background: "rgba(11, 13, 23, 0.5)",
+          borderRadius: "16px",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(3.1px)",
+          WebkitBackdropFilter: "blur(3.1px)",
+        }}
+      >
+        <ul className="flex px-5 py-8 gap-10 ml-10 text-[#fff]">
+          {navtext.map((list, index) => {
+            return <li key={index}>{list.title}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
