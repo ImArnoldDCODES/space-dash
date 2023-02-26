76d074aace379f82483891a22a4c50d1c800d35d
mustapha jay <mustaphajay2@gmail.com>
2023-02-26 14:55:15 +0100

    added remianing pages and paths for each page

--- src/pages/navbar/navbar.jsx
+++ src/pages/navbar/navbar.jsx
@@ -5,15 +5,19 @@ export default function Navbar() {
   const navtext = [
     {
       title: "00 HOME",
+      path: "/",
     },
     {
       title: "01 DESTINATION",
+      path: "/destination",
     },
     {
       title: "02 CREW",
+      path: "/crew",
     },
     {
       title: "03 TECHNOLOGY",
+      path: "/technology",
     },
   ];
 
@@ -22,8 +26,8 @@ export default function Navbar() {
       <div>
         <img src={Logo} alt="logo" className="w-[3rem] h-[3rem] ml-12" />
       </div>
-      <span className="h-[2px] w-[35rem] left-[24rem] opacity-[0.25] bg-[#fff] absolute content-none z-[1]"></span>
-      <div 
+      <span className="h-[1px] w-[35rem] left-[24rem] opacity-[0.25] bg-white absolute content-none z-[1]"></span>
+      <div
         className="w-[38rem] h-[100%]"
         style={{
           background: "rgba(11, 13, 23, 0.5)",
@@ -31,11 +35,16 @@ export default function Navbar() {
           boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
           backdropFilter: "blur(3.1px)",
           WebkitBackdropFilter: "blur(3.1px)",
-        }}
-      >
-        <ul className="flex px-5 py-8 gap-10 ml-10 text-[#fff]">
+        }}>
+        <ul className="flex px-5 py-8 gap-10 ml-10 text-white">
           {navtext.map((list, index) => {
-            return <li key={index}>{list.title}</li>;
+            return (
+              <div>
+                <li key={index}>
+                  <a href={list.path}>{list.title}</a>
+                </li>
+              </div>
+            );
           })}
         </ul>
       </div>
