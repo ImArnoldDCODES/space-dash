19b1c49b67b77398ac9687efa1b849b6d065b565
mustapha jay <mustaphajay2@gmail.com>
2023-03-01 21:54:35 +0100

    added responsiveness to navbar

--- src/pages/navbar/navbar.jsx
+++ src/pages/navbar/navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Logo from "../../assets/shared/logo.svg";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
+  const [active, setActive] = React.useState(false);
   const navtext = [
     {
       title: "00 HOME",
@@ -21,31 +23,50 @@ export default function Navbar() {
     },
   ];
 
+  const handleNav = () => {
+    console.log("clicked");
+    setActive(!active);
+  };
   return (
-    <div className="absolute flex w-[100%] justify-between items-center ">
-      <div>
-        <img src={Logo} alt="logo" className="w-[3rem] h-[3rem] ml-12" />
+    <>
+      <div className="absolute flex w-[100%] justify-between items-center mt-5">
+        <div>
+          <img
+            src={Logo}
+            alt="logo"
+            className="absolue top-[2rem] mt-2 z-[1] md:static w-[3rem] h-[3rem] ml-12"
+          />
+        </div>
+        <div className="w-[40rem] h-[100%] flex align-center justify-center nav">
+          <span className="hidden lg:block h-[1px] w-[30rem] right-[38rem] top-[2.7rem] opacity-[0.25] bg-[#fff] absolute content-none z-[1] m-auto"></span>
+          {/* "hidden md:flex px-5 py-8 gap-10 ml-10 text-[#fff]" */}
+          <ul className="hidden md:flex px-5 py-8 gap-10 ml-10 text-[#fff]">
+            {navtext.map((list, index) => {
+              return (
+                <li key={index}>
+                  <a href={list.path}>{list.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
-      <div
-        className="w-[40rem] h-[100%] flex align-center justify-center"
-        style={{
-          background: "rgba(11, 13, 23, 0.5)",
-          borderRadius: "16px",
-          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-          backdropFilter: "blur(3.1px)",
-          WebkitBackdropFilter: "blur(3.1px)",
-        }}>
-        <span className="h-[2px] w-[35rem] right-[38rem] top-[2.7rem] opacity-[0.25] bg-[#fff] absolute content-none z-[1] m-auto"></span>
-        <ul className="flex px-5 py-8 gap-10 ml-10 text-[#fff]">
-          {navtext.map((list, index) => {
-            return (
-              <li key={index}>
-                <a href={list.path}>{list.title}</a>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="absolute right-[2rem] top-[2rem] " onClick={handleNav}>
+        {active && (
+          <FaBars
+            className={`${
+              active ? "block md:hidden text-4xl text-white" : ""
+            } `}
+          />
+        )}
+        {!active && (
+          <FaTimes
+            className={`${
+              !active ? "block md:hidden text-4xl text-white" : ""
+            } `}
+          />
+        )}
       </div>
-    </div>
+    </>
   );
 }
