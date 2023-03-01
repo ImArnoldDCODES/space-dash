087b5d638751f92fa85bb18d8e60398207f45297
mustapha jay <mustaphajay2@gmail.com>
2023-03-01 15:50:33 +0100

    fixed destination

--- src/pages/destination.jsx
+++ src/pages/destination.jsx
@@ -1,108 +1,133 @@
-import { useState, useEffect } from "react";
-import Moon from "../assets/destination/image-moon.png";
+import React, { useState, useEffect } from "react";
+import moon from "../assets/destination/image-moon.png";
+import mars from "../assets/destination/image-mars.png";
+import europa from "../assets/destination/image-europa.png";
+import titan from "../assets/destination/image-titan.png";
+
+const planets = [
+  {
+    id: 1,
+    name: "MOON",
+    description:
+      " See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+    image: moon,
+    distance: "384,400 km",
+    time: "3 days",
+  },
+  {
+    id: 2,
+    name: "MARS",
+    description:
+      "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+    image: mars,
+    distance: "225 MIL. km",
+    time: "9 months",
+  },
+  {
+    id: 3,
+    name: "EUROPA",
+    description:
+      "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+    image: europa,
+    distance: "628 MIL. km",
+    time: "3 years",
+  },
+  {
+    id: 4,
+    name: "TITAN",
+    description:
+      "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+    image: titan,
+    distance: "1.6 BIL. km",
+    time: "7 years",
+  },
+];
+
+const links = [
+  { id: 1, name: "MOON" },
+  { id: 2, name: "MARS" },
+  { id: 3, name: "EUROPA" },
+  { id: 4, name: "TITAN" },
+];
 
 const Destination = () => {
-  // const [view, setView] = useState({
-  //   moon: false,
-  //   mars: false,
-  //   europa: false,
-  //   titan: false,
-  // });
+  const [index, setIndex] = React.useState(0);
 
-  const [moon, setMoon] = useState(true);
-  const [mars, setMars] = useState(false);
-  const [europa, setEuropa] = useState(false);
-  const [titan, setTitan] = useState(false);
-
-  const switchMoon = () => {
-    setMoon(true);
-    setMars(false);
+  const nextSlide = () => {
+    const isFirst = index === planets.length - 1;
+    const newSlide = isFirst ? 0 : index + 1;
+    setIndex(newSlide);
   };
-  const switchMars = () => {
-    setMoon(false);
-    setMars(!mars);
+  const prevSlide = () => {
+    const isLast = index === 0;
+    const newSlide = isLast ? planets.length - 1 : index - 1;
+    setIndex(newSlide);
   };
 
+  React.useEffect(() => {
+    const time = setTimeout(() => {
+      nextSlide();
+    }, 5000);
+    return () => clearTimeout(time);
+  }, [index]);
+
+  const gotToSlide = (slideIndex) => {
+    setIndex(slideIndex);
+  };
   return (
     <div className="h-[100vh] w-[100vw] overflow-x-hidden desitination-page">
-      <div className="flex h-[100%] m-auto mt-12">
-        <div className="basis-[50%] m-auto text-start mt-[13rem] pl-[7rem]">
-          <h4 className="text-[28px] text-[#fff] leading-normal tracking-[4px]">
+      <div className="grid lg:grid-cols-2 gap-8 m-auto mt-[12rem] ">
+        <div className=" m-auto text-start  ">
+          <h4 className="text-2xl text-center text-[#fff] leading-normal tracking-[4px]">
             <span className="opacity-[0.25] mr-5">01</span>
             PICK YOUR DESTINATION
           </h4>
           <img
-            src={Moon}
+            src={planets[index].image}
             alt=""
             width={100}
             height={100}
-            className="m-auto h-[22rem] w-[22rem] mt-20"
+            className="lg:ml-[8rem] w-[24rem] my-10 mx-auto"
           />
         </div>
-        <div className={"basis-[50%] m-auto mt-[17rem]"}>
+        <div className=" m-auto  ">
           <div className="flex flex-col w-fit m-auto">
-            <ul className="flex text-[#D0D6F9] gap-10 text-[16px] font-[400] cursor-pointer">
-              <li onClick={switchMoon}>MOON</li>
-              <li onClick={switchMars}>MARS</li>
-              <li onClick={() => setEuropa(true)}>EUROPA</li>
-              <li onClick={() => setTitan(true)}>TITAN</li>
+            <ul className="flex text-[#D0D6F9] gap-10 text-[16px] font-[400] cursor-pointer mx-auto lg:mx-0">
+              {links.map((link, index) => {
+                return (
+                  <li key={link.id} onClick={() => gotToSlide(index)}>
+                    {link.name}
+                  </li>
+                );
+              })}
             </ul>
 
-            <div className={moon ? "block" : "hidden"}>
-              <h4 className="text-[#fff] leading-normal text-[100px]">MOON</h4>
+            <div className="text-center lg:text-left">
+              <h4 className="text-white leading-normal my-5 text-5xl md:text-6xl">
+                {planets[index].name}
+              </h4>
               <p className="text-[#D0D6F9] text-[18px] leading-normal w-[28rem]">
-                See our planet as you’ve never seen it before. A perfect
-                relaxing trip away to help regain perspective and come back
-                refreshed. While you’re there, take in some history by visiting
-                the Luna 2 and Apollo 11 landing sites.
+                {planets[index].description}
               </p>
               <hr className="w-[100%] mt-10 border-[#383B4B]" />
-              <div className="flex gap-16 mt-7">
+              <div className="md:flex justify-center lg:justify-start gap-16 my-7">
                 <span>
                   <h5 className="text-[14px] text-[#D0D6F9] tracking-[2.3px] leading-normal">
                     AVG, DISTANCE
                   </h5>
-                  <h3 className="text-[#fff] text-[28px] leading-normal">
-                    384,400 KM
+                  <h3 className="text-[#fff] mb-6 text-[28px] leading-normal">
+                    {planets[index].distance}
                   </h3>
                 </span>
                 <span>
                   <h5 className="text-[14px] text-[#D0D6F9] tracking-[2.3px] leading-normal">
-                    AVG, DISTANCE
+                    AVG, Time
                   </h5>
                   <h3 className="text-[#fff] text-[28px] leading-normal">
-                    384,400 KM
+                    {planets[index].time}
                   </h3>
                 </span>
               </div>
-            </div>
-          </div>
-
-          <div className={mars ? "block border-2 border-[#fff] w-fit m-auto" : "hidden"}>
-            <h4 className="text-[#fff] leading-normal text-[100px]">MARS</h4>
-            <p className="text-[#D0D6F9] text-[18px] leading-normal w-[28rem]">
-              Don’t forget to pack your hiking boots. You’ll need them to tackle
-              Olympus Mons, the tallest planetary mountain in our solar system.
-              It’s two and a half times the size of Everest!
-            </p>
-            <hr className="w-[100%] mt-10 border-[#383B4B]" />
-            <div className="flex gap-16 mt-7">
-              <span>
-                <h5 className="text-[14px] text-[#D0D6F9] tracking-[2.3px] leading-normal">
-                  AVG, DISTANCE
-                </h5>
-                <h3 className="text-[#fff] text-[28px] leading-normal">
-                  225 MIL. KM
-                </h3>
-              </span>
-              <span>
-                <h5 className="text-[14px] text-[#D0D6F9] tracking-[2.3px] leading-normal">
-                  AVG, DISTANCE
-                </h5>
-                <h3 className="text-[#fff] text-[28px] leading-normal">
-                  9 MONTHS
-                </h3>
-              </span>
             </div>
           </div>
         </div>
