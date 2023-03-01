89eb8dc2b69c878057a6cf7978f44f64de06c30a
mustapha jay <mustaphajay2@gmail.com>
2023-03-01 14:37:30 +0100

    added crew nad technology pages

--- src/pages/crew.jsx
+++ src/pages/crew.jsx
@@ -1,29 +1,104 @@
 import React from "react";
 import douglas from "../assets/crew/image-douglas-hurley.png";
+import mark from "../assets/crew/image-mark-shuttleworth.png";
+import victor from "../assets/crew/image-victor-glover.png";
+import anousheh from "../assets/crew/image-anousheh-ansari.png";
+import { RxDotFilled } from "react-icons/rx";
+
+const crewlist = [
+  {
+    id: 1,
+    role: "Commander",
+    name: "Douglas Hurley",
+    description:
+      "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+    image: douglas,
+  },
+  {
+    id: 2,
+    role: "Mission Specialist ",
+    name: "MARK SHUTTLEWORTH",
+    description:
+      "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+    image: mark,
+  },
+  {
+    id: 3,
+    role: "PILOT",
+    name: "Victor Glover",
+    description:
+      "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
+    image: victor,
+  },
+  {
+    id: 4,
+    role: "Flight Engineer",
+    name: "Anousheh Ansari",
+    description:
+      "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.  ",
+    image: anousheh,
+  },
+];
 
 const crew = () => {
+  const [index, setIndex] = React.useState(0);
+
+  const nextSlide = () => {
+    const isFirst = index === crewlist.length - 1;
+    const newSlide = isFirst ? 0 : index + 1;
+    setIndex(newSlide);
+  };
+  const prevSlide = () => {
+    const isLast = index === 0;
+    const newSlide = isLast ? crewlist.length - 1 : index - 1;
+    setIndex(newSlide);
+  };
+
+  React.useEffect(() => {
+    const time = setTimeout(() => {
+      nextSlide();
+    }, 5000);
+    return () => clearTimeout(time);
+  }, [index]);
+
   return (
-    <div className="crew h-[100vh]">
-      <div className="lg:grid grid-cols-2 gap-10 items-center  justify-center pt-[7rem] crew">
-        <div className="text-white  text-center">
-          <h2 className="text-left text-lg m-8 lg:text-3xl lg:mb-20 lg:text-center">
-            <span className="opacity-50">02</span> Meet your crew{" "}
-          </h2>
-          <h1 className="text-4xl opacity-50 mb-2">Commander </h1>
-          <h1 className="text-5xl mb-8">Douglas Hurley</h1>
-          <p className="opacity-50 w-3/4 mx-auto">
-            Douglas Gerald Hurley is an American engineer, former Marine Corps
-            pilot and former NASA astronaut. He launched into space for the
-            third time as commander of Crew Dragon Demo-2.
-          </p>
+    <>
+      <div className="crew h-[100vh]">
+        <div className="grid lg:grid-cols-2 items-center  justify-center pt-[7rem] crew">
+          <div className="text-white m-4 text-center order-last md:order-first grid">
+            <h2 className="hidden md:block text-center md:text-left text-lg m-8 lg:text-3xl lg:mb-16 lg:text-center grid order-first">
+              <span className="opacity-50">02</span> Meet your crew{" "}
+            </h2>
+            <h1 className="text-3xl opacity-50 mb-2">
+              {crewlist[index].role}{" "}
+            </h1>
+            <h1 className="text-4xl mb-8">{crewlist[index].name}</h1>
+            <p className="opacity-50 w-[90%] mx-auto text-xl">
+              {crewlist[index].description}
+            </p>
+            <div className="flex justify-center py-2 cursor-pointer">
+              {crewlist.map((item, index) => {
+                return (
+                  <div key={index} className="text-3xl text-gray-700">
+                    <RxDotFilled onClick={() => setIndex(index)} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="grid">
+            <h2 className="block md:hidden text-center md:text-center text-lg m-8 lg:text-3xl lg:mb-16 lg:text-center text-white order-first">
+              <span className="opacity-50">02</span> Meet your crew{" "}
+            </h2>
+            <img
+              src={crewlist[index].image}
+              className="w-[300px] mx-auto my-8 order-first md:order-last"
+              alt=""
+            />
+          </div>
         </div>
-        <img
-          src={douglas}
-          className="w-[300px] lg:w-full mx-auto my-8 "
-          alt=""
-        />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/technology.jsx
+++ src/pages/technology.jsx
@@ -1,28 +1,120 @@
 import React from "react";
 import launch from "../assets/technology/image-launch-vehicle-portrait.jpg";
 import launch2 from "../assets/technology/image-launch-vehicle-landscape.jpg";
+import space2 from "../assets/technology/image-space-capsule-landscape.jpg";
+import space from "../assets/technology/image-space-capsule-portrait.jpg";
+import spaceport2 from "../assets/technology/image-spaceport-landscape.jpg";
+import spaceport from "../assets/technology/image-spaceport-portrait.jpg";
+import { RxDotFilled } from "react-icons/rx";
+
+const tech = [
+  {
+    id: 1,
+    role: "THE TERMINOLOGY… ",
+    name: "LAUNCH VEHICLE",
+    description:
+      " A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+    image: [
+      { id: 10, image1: launch },
+      { id: 20, image2: launch2 },
+    ],
+    value: 1,
+  },
+  {
+    id: 2,
+    role: "THE TERMINOLOGY…  ",
+    name: "SPACEPORT",
+    description:
+      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+    image: [
+      { id: 30, image1: spaceport },
+      { id: 40, image2: spaceport2 },
+    ],
+    value: 2,
+  },
+  {
+    id: 3,
+    role: "THE TERMINOLOGY…",
+    name: "SPACE CAPSULE",
+    description:
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+
+    image: [
+      { id: 50, image1: space },
+      { id: 60, image2: space2 },
+    ],
+    value: 3,
+  },
+];
 
 const technology = () => {
+  const [index, setIndex] = React.useState(0);
+  // const [value, setValue] = React.useState(0);
+  const [active, setActive] = React.useState(true);
+
+  const nextSlide = () => {
+    const isFirst = index === tech.length - 1;
+    const newSlide = isFirst ? 0 : index + 1;
+    setIndex(newSlide);
+  };
+  const prevSlide = () => {
+    const isLast = index === 0;
+    const newSlide = isLast ? tech.length - 1 : index - 1;
+    setIndex(newSlide);
+  };
+
+  React.useEffect(() => {
+    const time = setTimeout(() => {
+      nextSlide();
+    }, 5000);
+    return () => clearTimeout(time);
+  }, [index]);
+
+  const gotToSlide = (slideIndex) => {
+    setIndex(slideIndex);
+  };
+  let toggle = active ? "text-red-500" : null;
+
   return (
     <div className=" h-[100vh]">
       <div className="grid lg:grid-cols-2 gap-10 items-center  justify-center pt-[9rem] technology">
-        <div className="text-white my-6 text-center">
-          <h2 className="text-left text-lg m-8 lg:text-3xl lg:mb-20 lg:text-center">
-            <span className="opacity-50">02</span> SPACE LAUNCH 101{" "}
-          </h2>
-          <h1 className="text-4xl opacity-50 mb-2">THE TERMINOLOGY… </h1>
-          <h1 className="text-5xl mb-8">LAUNCH VEHICLE</h1>
-          <p className="opacity-50 w-3/4 mx-auto">
-            A launch vehicle or carrier rocket is a rocket-propelled vehicle
-            used to carry a payload from Earth's surface to space, usually to
-            Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful
-            in operation. Standing 150 metres tall, it's quite an awe-inspiring
-            sight on the launch pad!
-          </p>
+        <div className="lg:flex gap-5 mx-6">
+          <div className="flex lg:flex-col justify-center py-2 cursor-pointer">
+            {tech.map((item, index) => {
+              return (
+                <div key={index} className="text-4xl text-gray-700">
+                  <RxDotFilled onClick={() => setIndex(index)} />
+                </div>
+              );
+            })}
+          </div>
+          <div className="text-white my-6 text-center">
+            <h2 className="hidden lg:block text-left text-lg m-8 lg:text-3xl lg:mb-20 lg:text-center">
+              <span className="opacity-50">02</span> SPACE LAUNCH 101{" "}
+            </h2>
+            <h1 className="text-2xl md:text-4xl opacity-50 mb-2">
+              {tech[index].role}
+            </h1>
+            <h1 className="text-3xl md:text-5xl mb-8">{tech[index].name}</h1>
+            <p className="opacity-50 w-3/4 mx-auto">
+              {tech[index].description}
+            </p>
+          </div>
         </div>
         <div className="order-first lg:order-last">
-          <img src={launch} className="hidden lg:grid mx-auto my-8 " alt="" />
-          <img src={launch2} className=" lg:hidden w-full my-8 " alt="" />
+          <h2 className="block lg:hidden text-center md:text-left text-lg m-8 lg:text-3xl text-white m-x-4 lg:mb-20 lg:text-center">
+            <span className="opacity-50">02</span> SPACE LAUNCH 101{" "}
+          </h2>
+          <img
+            src={tech[index].image[0].image1}
+            className="hidden lg:grid mx-auto my-8 "
+            alt=""
+          />
+          <img
+            src={tech[index].image[1].image2}
+            className=" lg:hidden w-full my-8 "
+            alt=""
+          />
         </div>
       </div>
     </div>
